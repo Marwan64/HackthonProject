161caf28ca16bcf0683bb5db5e103cf8c0b863fe
Alexander Minascurta <a.minascurta@wsu.edu>
2025-02-15 16:29:18 -0800

    Added levels 2 and 3 for the addition problems.

--- functions.c
+++ functions.c
@@ -1,8 +1,8 @@
 
 
 int addition_problem_level_1(int* score) {
-	int num1 = rand() % 10 + 1;
-	int num2 = rand() % 10 + 1;
+	int num1 = rand() % 9 + 1;
+	int num2 = rand() % 9 + 1;
 
 	int answer = num1 + num2;
 	printf("The problem is %d + %d, what is the answer? \n");
@@ -10,6 +10,46 @@ int addition_problem_level_1(int* score) {
 	scanf(%d, &reponce);
 	if (respince == answer) {
 		&score += 5;
+		printf("Correct! \n");
+		return 1;
+	}
+	else
+	{
+		printf("Sorry that incorrect!\n");
+		return 0;
+	}
+}
+
+int addition_problem_level_2(int* score) {
+	int num1 = rand() % 99 + 1;
+	int num2 = rand() % 99 + 1;
+
+	int answer = num1 + num2;
+	printf("The problem is %d + %d, what is the answer? \n");
+	int reponce;
+	scanf(% d, &reponce);
+	if (respince == answer) {
+		&score += 10;
+		printf("Correct! \n");
+		return 1;
+	}
+	else
+	{
+		printf("Sorry that incorrect!\n");
+		return 0;
+	}
+}
+
+int addition_problem_level_3(int* score) {
+	int num1 = rand() % 999 + 1;
+	int num2 = rand() % 999 + 1;
+
+	int answer = num1 + num2;
+	printf("The problem is %d + %d, what is the answer? \n");
+	int reponce;
+	scanf(% d, &reponce);
+	if (respince == answer) {
+		&score += 20;
 		printf("Correct! \n");
 		return 1;
 	}
